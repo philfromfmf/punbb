d3db44ba4d03bf4a23b06fc9f3e3fdd41d4d0a68
Albert Stepantsev <philfromfmf@mail.ru>
2013-11-11 23:01:58 +0400

    Invites extension development - bug fix

--- extensions/profit_invites/profile_navigation.php
+++ extensions/profit_invites/profile_navigation.php
@@ -7,9 +7,15 @@ if ( $forum_config['p_regs_invites'] ) {
     else
         include $ext_info['path'] . '/lang/English/pun_repository.php';
 
-    $adminMenu = $forum_page['main_menu']['admin'];
-    unset($forum_page['main_menu']['admin']);
+    if ( isset($forum_page['main_menu']['admin']) ) {
+        $adminMenu = $forum_page['main_menu']['admin'];
+        unset($forum_page['main_menu']['admin']);
+    }
+
     $forum_page['main_menu']['invites'] = '<li class="'.(($section == 'invites') ? ' active' : '').'"><a href="'.forum_link('profile.php?section=invites&amp;id=$1', $id).'"><span>'.$lang_profit_invites['Section invites'].'</span></a></li>';
-    $forum_page['main_menu']['admin'] = $adminMenu;
+
+    if ( isset($forum_page['main_menu']['admin']) ) {
+        $forum_page['main_menu']['admin'] = $adminMenu;
+    }
 
 }
