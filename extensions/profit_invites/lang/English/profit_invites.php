ec9c74e28c4766aac6a0e7003c6c93dda2109a76
Albert Stepantsev <philfromfmf@mail.ru>
2013-11-12 00:47:18 +0400

    Invites extension development - invite email is added

--- extensions/profit_invites/lang/English/profit_invites.php
+++ extensions/profit_invites/lang/English/profit_invites.php
@@ -21,6 +21,23 @@ $lang_profit_invites = array(
     'Email to invite' => 'User e-mail you want to invite',
     'Send invite' => 'Send invite',
 
+    'Mail subject' => 'You are welcome to <board_title>!',
+    'Mail body' => <<<BODY
+You are invited to in the forums at <base_url>.
+
+You have to register. To register, please visit the following page:
+<activation_url_with_code>
+
+If this url doesn't work, visit <activation_url> and manually enter you data
+e-mail: <email>
+invite code: <code>
+
+--
+<board_mailer>
+(Do not reply to this message)
+BODY
+,
+
     'Invite code' => 'Invite code',
     'Invite code help' => 'Enter invite code from invitation e-mail',
     'Invite doesn\'t exists' => 'Invite doesn\'t exists',
--- extensions/profit_invites/profile_db_update.php
+++ extensions/profit_invites/profile_db_update.php
@@ -2,6 +2,11 @@
 
 if (isset($_POST['form_sent']) && $section == 'invites' && empty($errors)) {
 
+    if (file_exists($ext_info['path'] . '/lang/' . $forum_user['language'] . '/profit_invites.php'))
+        include $ext_info['path'] . '/lang/' . $forum_user['language'] . '/profit_invites.php';
+    else
+        include $ext_info['path'] . '/lang/English/pun_repository.php';
+
     $code = mt_rand(100000, 999999);
 
     $query = array(
@@ -10,6 +15,17 @@ if (isset($_POST['form_sent']) && $section == 'invites' && empty($errors)) {
         'VALUES'    => $id . ',\'' . $form['invite_email'] . '\', ' . $code,
     );
     $forum_db->query_build($query) or error(__FILE__, __LINE__);
+
+    $mail_subject = $lang_profit_invites['Mail subject'];
+    $mail_message = $lang_profit_invites['Mail body'];
+    $mail_message = str_replace('<board_title>', $forum_config['o_board_title'], $mail_message);
+    $mail_message = str_replace('<base_url>', $base_url.'/', $mail_message);
+    $mail_message = str_replace('<activation_url_with_code>', $base_url.'/register.php?email=' . $form['invite_email'] . '&code=' . $code, $mail_message);
+    $mail_message = str_replace('<activation_url>', $base_url.'/register.php', $mail_message);
+    $mail_message = str_replace('<email>', $form['invite_email'], $mail_message);
+    $mail_message = str_replace('<code>', $code, $mail_message);
+    $mail_message = str_replace('<board_mailer>', sprintf($lang_common['Forum mailer'], $forum_config['o_board_title']), $mail_message);
+    forum_mail($form['invite_email'], $mail_subject, $mail_message);
 
     $forum_flash->add_info($lang_profile['Profile redirect']);
     redirect(forum_link('profile.php?section=invites&amp;id=$1', $id), $lang_profile['Profile redirect']);
