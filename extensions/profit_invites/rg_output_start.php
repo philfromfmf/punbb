c6b98397cc1309067d4bfb20cbbfbdbd8b09bf43
Albert Stepantsev <philfromfmf@mail.ru>
2013-11-19 20:01:23 +0400

    Invites extension development - minor fix

--- extensions/profit_invites/rg_output_start.php
+++ extensions/profit_invites/rg_output_start.php
@@ -1,8 +1,15 @@
 <?php
 
 if ( $forum_config['p_regs_invites'] ) {
+
     if ( !empty($_GET['req_email1']) )
         $_POST['req_email1'] = $_GET['req_email1'];
+    elseif ( !empty($_GET['email']) )
+        $_POST['req_email1'] = $_GET['email'];
+
     if ( !empty($_GET['req_invite_code']) )
         $_POST['req_invite_code'] = $_GET['req_invite_code'];
+    elseif ( !empty($_GET['code']) )
+        $_POST['req_invite_code'] = $_GET['code'];
+
 }
